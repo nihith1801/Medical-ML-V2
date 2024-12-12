228b142d547ef973c04b63e9b6f8813e7de8b557
nihith1801 <122144472+nihith1801@users.noreply.github.com>
2024-12-12 20:58:46 +0530

    Update page.tsx

--- app/mri/page.tsx
+++ app/mri/page.tsx
@@ -83,7 +83,7 @@ export default function MRIPage() {
               isDarkMode ? "text-white" : "text-black"
             )}
           >
-            MRI Analysis Model
+            Brain MRI Model
           </h1>
           
           <p 
@@ -93,7 +93,7 @@ export default function MRIPage() {
               isDarkMode ? "text-gray-300" : "text-gray-700"
             )}
           >
-            Our advanced MRI Analysis model utilizes deep learning techniques to analyze MRI scans,
+            Our advanced Brain MRI Analysis model utilizes deep learning techniques to analyze MRI scans,
             helping detect various abnormalities including brain tumors. It provides rapid and accurate insights
             to support medical professionals in their diagnostic process.
           </p>
